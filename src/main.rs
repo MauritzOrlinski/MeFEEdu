f98098c1a91e6e880c4029d57272a83845b9c4b5
Fox <No>
2025-10-22 02:03:14 +0200

    Add dynamic camera view, draw original structure at lower opacity

--- src/main.rs
+++ src/main.rs
@@ -49,10 +49,50 @@ fn draw_polygon_filled(d: &mut RaylibDrawHandle, points: &[RaylibVector2], color
     }
 }
 
-fn construct_ffi_vec(v: &vector2::Vector2) -> raylib::ffi::Vector2 {
+fn to_display(v: &vector2::Vector2) -> raylib::ffi::Vector2 {
     raylib::ffi::Vector2 {
         x: v.x as f32,
-        y: v.y as f32,
+        y: -v.y as f32,
+    }
+}
+
+fn get_camera<'a, I>(mut points: I) -> Camera2D
+where
+    I: Iterator<Item = &'a Vector2>,
+{
+    let first = points
+        .next()
+        .expect("Point list must at least contain one point");
+
+    let (x_axis, y_axis) = points.fold(
+        ((first.x, first.x), (first.y, first.y)),
+        |((x_min, x_max), (y_min, y_max)), point| {
+            (
+                (x_min.min(point.x), x_max.max(point.x)),
+                (y_min.min(point.y), y_max.max(point.y)),
+            )
+        },
+    );
+
+    let (x_min, x_max) = x_axis;
+    let x_extent = x_max - x_min;
+
+    let (y_min, y_max) = y_axis;
+    let y_extent = y_max - y_min;
+    let max_x_zoom = (W as f64) / x_extent;
+    let max_y_zoom = (H as f64) / y_extent;
+
+    let zoom = max_y_zoom.min(max_x_zoom) as f32;
+    let position = RaylibVector2::new(
+        (x_min + x_extent / 2.) as f32,
+        -(y_min + y_extent / 2.) as f32,
+    );
+
+    Camera2D {
+        target: position,
+        offset: RaylibVector2::new(W as f32 / 2., H as f32 / 2.),
+        rotation: 0.,
+        zoom: (zoom * 0.9),
     }
 }
 
@@ -71,45 +111,60 @@ fn main() {
     println!("Displacements: {solution:?}");
 
     let structure = beam_sim.structure;
-    let displaced_points: Vec<Vector2> = (0..structure.points.len())
-        .map(|n| {
-            (Vector2 {
-                x: solution[2 * n],
-                y: solution[2 * n + 1],
-            } + structure.points[n])
-                * -1.0
-        })
+
+    let displaced_points: Vec<_> = solution
+        .chunks_exact(2)
+        .zip(structure.points.iter())
+        .map(|(values, &vector)| vector + Vector2::new(values[0], values[1]))
         .collect();
 
     let (mut rl, thread) = raylib::init().size(W, H).title("").build();
-    let camera = Camera2D {
-        offset: RaylibVector2::new((W / 2) as f32, (H / 2) as f32),
-        target: RaylibVector2::new(0.0, 0.0),
-        rotation: 0.0,
-        zoom: 1.0,
-    };
+
+    let camera = get_camera(displaced_points.iter().chain(structure.points.iter()));
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(BACKGROUND);
+
         let mut mode = d.begin_mode2D(camera);
         for &(a, b) in &structure.connections {
             mode.draw_line_v(
-                construct_ffi_vec(&displaced_points[a]),
-                construct_ffi_vec(&displaced_points[b]),
+                to_display(&structure.points[a]),
+                to_display(&structure.points[b]),
+                EDGE_COLOR.lerp(BACKGROUND, 0.75),
+            );
+        }
+
+        for (n, v) in structure.points.iter().enumerate() {
+            if structure.dbc.contains_key(&n) {
+                mode.draw_circle_v(
+                    to_display(v),
+                    RADIUS / camera.zoom,
+                    STABLE_NODE_COLOR.lerp(BACKGROUND, 0.75),
+                );
+            } else {
+                mode.draw_circle_v(
+                    to_display(v),
+                    RADIUS / camera.zoom,
+                    NODE_COLOR.lerp(BACKGROUND, 0.75),
+                );
+            }
+        }
+
+        for &(a, b) in &structure.connections {
+            mode.draw_line_v(
+                to_display(&displaced_points[a]),
+                to_display(&displaced_points[b]),
                 EDGE_COLOR,
             );
         }
-        for v in &displaced_points {
-            mode.draw_circle_v(construct_ffi_vec(v), RADIUS, NODE_COLOR);
-        }
 
-        for &v in structure.dbc.keys() {
-            mode.draw_circle_v(
-                construct_ffi_vec(&displaced_points[v]),
-                RADIUS,
-                STABLE_NODE_COLOR,
-            );
+        for (n, v) in displaced_points.iter().enumerate() {
+            if structure.dbc.contains_key(&n) {
+                mode.draw_circle_v(to_display(v), RADIUS / camera.zoom, STABLE_NODE_COLOR);
+            } else {
+                mode.draw_circle_v(to_display(v), RADIUS / camera.zoom, NODE_COLOR);
+            }
         }
     }
 }
